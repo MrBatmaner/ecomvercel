01c4e17fdac80aa3046910a0004db1ed1d9a0c08
Your Name <you@example.com>
2023-09-10 14:48:41 +1000

    day 3

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC2-0E8EEeHgzYnrZ2qEB2Io5B6gQaEIOc",
+    authDomain: "ecommercestore-bade3.firebaseapp.com",
+    projectId: "ecommercestore-bade3",
+    storageBucket: "ecommercestore-bade3.appspot.com",
+    messagingSenderId: "853127698680",
+    appId: "1:853127698680:web:a90064baf2b7ddf6a2430d",
+    measurementId: "G-J3LLZ8H9G0"
+  };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,21 @@
-import { Provider } from 'react-redux'
-import { store } from '../app/store'
-import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react";
+import { Provider } from 'react-redux';
+import { store } from '../app/store';
+import '../styles/globals.css';
+import { SessionProvider as AuthProvider } from "next-auth/react";
 
-const MyApp = ({ Component, pageProps }) => {
+
+export default function App ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  )
-}
-
-export default MyApp
+    <AuthProvider session={pageProps.session}>
+      <Provider store={store}>
+        <SessionProvider session={pageProps.session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </Provider>
+    </AuthProvider>
+  );
+};
