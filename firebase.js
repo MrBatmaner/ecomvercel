4f8891e7c468d3336201aae4195c9a1b65ced6cb
Your Name <you@example.com>
2023-09-15 12:11:56 +1000

    theend

--- firebase.js
+++ firebase.js
@@ -2,11 +2,19 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC2-0E8EEeHgzYnrZ2qEB2Io5B6gQaEIOc",
-    authDomain: "ecommercestore-bade3.firebaseapp.com",
-    projectId: "ecommercestore-bade3",
-    storageBucket: "ecommercestore-bade3.appspot.com",
-    messagingSenderId: "853127698680",
-    appId: "1:853127698680:web:a90064baf2b7ddf6a2430d",
-    measurementId: "G-J3LLZ8H9G0"
-  };
+  apiKey: "AIzaSyC2-0E8EEeHgzYnrZ2qEB2Io5B6gQaEIOc",
+  authDomain: "ecommercestore-bade3.firebaseapp.com",
+  projectId: "ecommercestore-bade3",
+  storageBucket: "ecommercestore-bade3.appspot.com",
+  messagingSenderId: "853127698680",
+  appId: "1:853127698680:web:a90064baf2b7ddf6a2430d",
+  measurementId: "G-J3LLZ8H9G0"
+};
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+
+export default db;
--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
   images: {
     domains: ["links.papareact.com", "fakestoreapi.com", "drive.google.com", "www.pngitem.com", "www.pngitem.com", "joker-uploads.s3.ap-southeast-2.amazonaws.com"],
   },
+  env: {
+    stripe_public_key: process.env.STRIPE_PUBLIC_KEY
+  },
 };
